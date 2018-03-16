cb1f25e450df3fcde210fcfd22da022e1b846e13
amurillojr92 <amurillojr92@gmail.com>
2018-03-16 19:35:30 -0400

    added carts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
+  get 'static_pages/about'
+
+  resources :line_items
+  resources :carts
+  get 'store/index'
+
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'products#index'
 end
